c3ac10b139e0a0457a9abec4680d543f230513cb
unknown <khaledbadawy315@gmail.com>
2024-10-25 22:32:09 +0300

    last-edit-fridy

--- src/main.js
+++ src/main.js
@@ -1,3 +1,50 @@
+const image = document.getElementById("image");
+const lens = document.getElementById("lens");
+const zoomResult = document.getElementById("zoomResult");
+const zoomedImage = document.getElementById("zoomedImage");
+
+image.addEventListener("mousemove", moveLens);
+lens.addEventListener("mousemove", moveLens);
+image.addEventListener("mouseleave", function () {
+  lens.style.display = "none";
+  zoomResult.style.display = "none";
+});
+
+function moveLens(e) {
+  lens.style.display = "block";
+  zoomResult.style.display = "block";
+
+  const pos = getCursorPos(e);
+  let x = pos.x - lens.offsetWidth / 2;
+  let y = pos.y - lens.offsetHeight / 2;
+
+  if (x > image.width - lens.offsetWidth) x = image.width - lens.offsetWidth;
+  if (x < 0) x = 0;
+  if (y > image.height - lens.offsetHeight)
+    y = image.height - lens.offsetHeight;
+  if (y < 0) y = 0;
+
+  lens.style.left = x + "px";
+  lens.style.top = y + "px";
+
+  zoomedImage.style.left = -(x * 2) + "px";
+  zoomedImage.style.top = -(y * 2) + "px";
+}
+
+function getCursorPos(e) {
+  const rect = image.getBoundingClientRect();
+  const x = e.pageX - rect.left - window.pageXOffset;
+  const y = e.pageY - rect.top - window.pageYOffset;
+  return { x: x, y: y };
+}
+image.addEventListener("mouseleave", hideLensAndZoom);
+lens.addEventListener("mouseleave", hideLensAndZoom);
+
+function hideLensAndZoom() {
+  lens.style.display = "none";
+  zoomResult.style.display = "none";
+}
+
 swiper = new Swiper(".swiper", {
   loop: true,
   pagination: {
@@ -160,6 +207,19 @@ swiper = new Swiper(".swiper-add", {
     },
   },
 });
+swiper = new Swiper(".mobile-swiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    spaceBetween: 500,
+  },
+
+  // autoplay: {
+  //   delay: 3000,
+  //   disableOnInteraction: false,
+  // },
+});
 
 document
   .querySelector(".categories-toggle")
@@ -228,3 +288,55 @@ function changeImage(newSrc) {
   document.getElementById("image").src = newSrc;
   document.getElementById("zoomedImage").src = newSrc;
 }
+
+// start favorite card
+function toggleFavorite() {
+  const heartIcon = document.getElementById("heartIcon");
+  const wishListIcon = document.getElementById("wishListIcon");
+
+  if (heartIcon.classList.contains("fa-heart-o")) {
+    heartIcon.classList.remove("fa-heart-o");
+    heartIcon.classList.add("fa-heart");
+    heartIcon.style.color = "red";
+
+    wishListIcon.classList.remove("fa-heart-o");
+    wishListIcon.classList.add("fa-heart");
+    wishListIcon.style.color = "black";
+  } else {
+    heartIcon.classList.remove("fa-heart");
+    heartIcon.classList.add("fa-heart-o");
+    heartIcon.style.color = "";
+
+    wishListIcon.classList.remove("fa-heart");
+    wishListIcon.classList.add("fa-heart-o");
+    wishListIcon.style.color = "";
+  }
+}
+// end favorite card
+// start toastr
+function showToast() {
+  const toast = document.getElementById("toast");
+  const toastTimer = document.getElementById("toast-timer");
+  toast.style.display = "block";
+
+  // Reset the width of the timer to 100%
+  toastTimer.style.width = "100%";
+
+  // Reduce the width of the timer over 5 seconds
+  setTimeout(() => {
+    toastTimer.style.transition = "width 5s linear";
+    toastTimer.style.width = "0%";
+  }, 0);
+
+  // Hide the toast after 5 seconds
+  setTimeout(hideToast, 5000);
+}
+
+function hideToast() {
+  const toast = document.getElementById("toast");
+  const toastTimer = document.getElementById("toast-timer");
+  toast.style.display = "none";
+  toastTimer.style.transition = "none"; // Reset the transition for next use
+}
+
+// end toastr
